9686e589e3626233b7fb338894a40168d73117b0
Nick <nikmaunt@gmail.com>
2023-02-03 12:32:01 +0300

    main background image added

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -6,7 +6,7 @@ import {FaGithub, FaLinkedin, FaTelegram} from "react-icons/all";
 const Main = () => {
     return (
         <div id='main'>
-            <img className='w-full h-screen object-cover object-right scale-x-[-1]' src={bgPhoto} alt=""/>
+            <img className='w-full h-screen object-cover object-right scale-x-[ya1]' src={bgPhoto} alt=""/>
             <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
                 <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
                     <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>I'm Nikita Kokhan</h1>
